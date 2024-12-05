2b085de9db5331cbe0c1584be01c106336400d30
Anirban Karmakar <anirbankarmakar0190@gmail.com>
2024-12-05 22:33:43 +0530

    First push

---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import Main from "@/pages/Main";
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
