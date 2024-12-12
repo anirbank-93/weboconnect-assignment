8073e2eacb2507a30bbc772f514fc971898b4a89
Anirban Karmakar <anirbankarmakar0190@gmail.com>
2024-12-13 04:34:57 +0530

    navbar

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -32,8 +32,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        <ReduxProvider>{children}</ReduxProvider>
+        <ReduxProvider>
+          <Navbar />
+          {children}
+        </ReduxProvider>
       </body>
     </html>
   );
